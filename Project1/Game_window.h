dad00450f99f2c44e21c2e089d7ac052eeeb9358
eMoDe69 <emode69@outlook.com>
2023-05-03 22:04:23 +0200

    klasa okna gry

---
+++ Project1/Game_window.h
@@ -0,0 +1,11 @@
+#ifndef GAME_WINDOW_H
+#define GAME_WINDOW_H
+
+class Game_window
+{
+
+};
+
+
+
+#endif // !GAME_WINDOW_H
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include "Game_window.h"
 
 int main(int argc, char* argv[])
 {
