5012f646a45e90d9e0a4337eb50e710804d97ea2
Filip <fili58429@gmail.com>
2023-05-18 16:25:09 +0200

    poczatek metody update_enemies

--- sfml2.0/game.cpp
+++ sfml2.0/game.cpp
@@ -4,3 +4,8 @@ void Game::update_mouse_pos()
 	this->mouse_pos_window = sf::Mouse::getPosition(*this->window);
 	this->mouse_pos_view = this->window->mapPixelToCoords(this->mouse_pos_window);
 }
+void Game::update_enemies()
+{
+	//updating the timer for enemy spawning
+	if (this->enemies.size() < this->max_enemies)
+	{
