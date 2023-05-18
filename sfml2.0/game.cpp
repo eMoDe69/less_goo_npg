85308563b967174e184ba41556d48915e928510e
Konrad Czerniej <czerniej@student.agh.edu.pl>
2023-05-18 19:32:56 +0200

    render_enemies

--- sfml2.0/game.cpp
+++ sfml2.0/game.cpp
@@ -1,7 +1,14 @@
 void Game::update_text()
 {
-
 	std::stringstream ss;
 	ss << "Points: " << this->points;
 	this->text.setString(ss.str());
+}
+
+void Game::render_enemies()
+{
+	for (auto& e : this->enemies)
+	{
+		this->window->draw(e);
+	}
 }
