87c46e85b7cc97e18e047ab441f9b4b41a6b3f91
Jedrzej Cypryjanski <cypra.j@gmail.com>
2023-05-19 13:18:51 +0200

    dodanie kodu w pliku zrodlowym

--- sfml2.0/game.cpp
+++ sfml2.0/game.cpp
@@ -7,3 +7,18 @@ Game::Game()
 	this->init_texts();
 	this->init_enemies();
 }
+Game::~Game()
+{
+	delete this->window;
+}
+
+//accessors
+const bool Game::running() const
+{
+	return this->window->isOpen();
+}
+
+const bool Game::get_end_game() const
+{
+	return this->end_game;
+}
