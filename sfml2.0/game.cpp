1bac0b498c598b75db90661acfd0b3513eb6795c
Jedrzej Cypryjanski <cypra.j@gmail.com>
2023-05-20 16:56:10 +0200

    dodanie kodu pliku źródłowego

--- sfml2.0/game.cpp
+++ sfml2.0/game.cpp
@@ -22,3 +22,17 @@ const bool Game::get_end_game() const
 {
 	return this->end_game;
 }
+
+//functions
+void Game::spawn_enemy()
+{
+	this->enemy.setPosition(
+		static_cast<float>(rand() % static_cast<int>(this->window->getSize().x - this->enemy.getSize().x)),
+		-10.f
+	);
+	int a = rand() % (256);
+	int b = rand() % (256);
+	int c = rand() % (256);
+	this->enemy.setFillColor(sf::Color(a, b, c, 255));
+	this->enemies.push_back(this->enemy);
+}
