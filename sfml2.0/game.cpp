08076452c6d28a215eb9fab294442db171378f0b
eMoDe69 <emode69@outlook.com>
2023-05-21 16:24:20 +0200

    2

--- sfml2.0/game.cpp
+++ sfml2.0/game.cpp
@@ -4,4 +4,12 @@
 
 //private functions
 void Game::init_variables()
-{}
+{
+	this->window = nullptr;
+
+	//game logic
+	this->points = 0;
+	this->enemy_spawn_timer_max = 40.f;
+	this->enemy_spawn_timer = this->enemy_spawn_timer_max;
+	this->max_enemies = 10;
+}
