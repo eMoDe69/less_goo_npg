fe1a06ac3de2715fc70161678c0d4e75f920552b
Michal Filipkowski <michal.filipkowski7@gmail.com>
2023-05-24 11:41:27 +0200

    ostatnia aktualizacja

--- sfml2.0/game.h
+++ sfml2.0/game.h
@@ -31,5 +31,50 @@ private:
 
 	//text
 	sf::Text text;
+
+	//game objects
+	std::vector<sf::RectangleShape> enemies;
+	sf::RectangleShape enemy;
+
+	//game logic
+	bool end_game;
+	unsigned points;
+	int health;
+	float enemy_spawn_timer;
+	float enemy_spawn_timer_max;
+	int max_enemies;
+	bool mouse_held;
+	float move_time;
+	float move_time_max;
+	float speed1;
+	float speed2;
+
+	//private functions
+	void init_variables();
+	void init_window();
+	void init_enemies();
+	void init_fonts();
+	void init_texts();
+public:
+	//const/dest
+	Game();
+	~Game();
+
+	//accessors
+	const bool running() const;
+	const bool get_end_game() const;
+
+	//public functions
+	void spawn_enemy();
+	void poll_events();
+	void update_mouse_pos();
+
+	void update_enemies();
+	void update();
+	void update_text();
+
+	void render_enemies();
+	void render();
+	void render_text(sf::RenderTarget& target);
 };    
 #endif
